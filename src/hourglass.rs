4553778126362233ab6382269df516f13795811b
kiki <mullerkrisztianbusiness@gmail.com>
2025-03-22 11:53:01 +0100

    Command line arguments

--- src/hourglass.rs
+++ src/hourglass.rs
@@ -83,7 +83,8 @@ impl<T> std::ops::IndexMut<(usize, usize)> for Grid<T> {
 
 pub struct Hourglass {
     layout: Grid<LayoutCell>,
-    state: Grid<u8>
+    state: Grid<u8>,
+    pinched: bool
 }
 
 impl Hourglass {
@@ -100,6 +101,7 @@ impl Hourglass {
         Hourglass {
             layout,
             state: Grid::<u8>::new(width, height, || 0),
+            pinched: false
         }
     }
 
@@ -139,6 +141,31 @@ impl Hourglass {
         }
     }
 
+    pub fn get_interior_positions(&self) -> Vec<(usize, usize)> {
+        let mut vec = Vec::<(usize, usize)>::new();
+
+        for y in 0..self.height() {
+            let mut interior_reached = false;
+            for x in 0..self.width() {
+                let empty_here = if let LayoutCell::Empty = self.layout[(x, y)] { true } else { false };
+
+                if interior_reached {
+                    if empty_here {
+                        vec.push((x, y));
+                    } else {
+                        break; // End row
+                    }
+                } else {
+                    if !empty_here {
+                        interior_reached = true;
+                    }
+                }
+            }
+        }
+
+        vec
+    }
+
 
     pub fn width(&self) -> usize {
         self.layout.width()
@@ -146,6 +173,19 @@ impl Hourglass {
 
     pub fn height(&self) -> usize {
         self.layout.height()
+    }
+
+
+    pub fn pinched(&self) -> bool {
+        self.pinched
+    }
+
+    pub fn pinch(&mut self) {
+        self.pinched = true;
+    }
+
+    pub fn unpinch(&mut self) {
+        self.pinched = false;
     }
 
 
@@ -170,15 +210,68 @@ impl Hourglass {
         }
     }
 
-    pub fn try_add_sand(&mut self) -> bool {
-        self.try_place_sand((self.width() / 2, 1))
+    pub fn fill_with_sand_from_top(&mut self, fullness: f32) {
+        let positions: Vec<(usize, usize)> = self.get_interior_positions();
+        let mut grains_left: usize = (((positions.len() * Hourglass::MAX_CELL_SAND as usize) as f32) * fullness) as usize;
+
+        for pos in positions {
+            let grains: usize = std::cmp::min(grains_left, Hourglass::MAX_CELL_SAND.into());
+            grains_left -= grains;
+            self.state[pos] += grains as u8;
+
+            if grains_left == 0 {
+                break;
+            }
+        }
+    }
+
+    /// Advances state until nothing changes for a while. Returns the number of advancements.
+    pub fn settle_state(&mut self, rng: &mut impl rand::Rng) -> u64 {
+        let mut steps: u64 = 0;
+        let mut inactive_for = 0;
+
+        while inactive_for < 16 {
+            let moves = self.advance(rng);
+            if moves == 0 {
+                inactive_for += 1;
+            } else {
+                inactive_for = 0;
+            }
+
+            steps += 1
+        }
+
+        steps
     }
 
 
+    pub fn count_sand(&self, range_x: std::ops::Range<usize>, range_y: std::ops::Range<usize>) -> usize {
+        let mut count: usize = 0;
+
+        for y in range_y {
+            for x in range_x.clone() {
+                count += self.state[(x, y)] as usize;
+            }
+        }
+
+        count
+    }
+
+    pub fn count_top_sand(&self) -> usize {
+        self.count_sand(0..self.width(), 0..(self.height() / 2))
+    }
+
+    pub fn count_bottom_sand(&self) -> usize {
+        self.count_sand(0..self.width(), (self.height() / 2)..self.height())
+    }
+
+
+    /// Advances state once. Returns the number of grain movements.
     pub fn advance(&mut self, rng: &mut impl rand::Rng) -> usize {
         let mut moves: usize = 0;
 
         for y in (0..(self.height())).rev() {
+            let skip_down_this_row = self.pinched() && (y == self.height() / 2 - 1);
             for x in 0..self.width() {
                 let here = (x, y);
                 assert!(self.state[here] <= Hourglass::MAX_CELL_SAND);
@@ -189,6 +282,10 @@ impl Hourglass {
                     2 => MoveDirection::Left,
                     _ => panic!()
                 };
+
+                if skip_down_this_row && (if let MoveDirection::Down = dir { true } else { false }) {
+                    continue;
+                }
 
                 if self.can_flow(&here, &dir) {
                     self.state[here] -= 1;
@@ -211,6 +308,7 @@ impl Hourglass {
     }
 
 
+    // TODO liquid sand when one bulb has to be fully full
     fn can_flow(&self, pos: &(usize, usize), dir: &MoveDirection) -> bool {
         assert!(self.state.is_in_bounds(*pos));
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,192 @@
 pub mod hourglass;
 
+use clap::Parser;
+use chrono::{NaiveDateTime, NaiveTime, TimeDelta, ParseResult};
 use hourglass::Hourglass;
 
 
-fn main() {
-    let mut glass = Hourglass::new(7, 12);
-    let mut sand_to_add = 30;
-    let mut no_moves_since = 0;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Start of time range. (today)
+    #[arg(long)]
+    begin: Option<String>,
+
+    /// End of time range. (if this is less than begin, it's interpreted to be tomorrow)
+    #[arg(long)]
+    end: Option<String>,
+
+    /// Length of time range. (for example, 90s, 1m30s, 1y2d3h4m5s)
+    #[arg(long)]
+    length: Option<String>,
+
+    /// Total width of the hourglass. (must be odd)
+    #[arg(long, default_value_t = 7)]
+    width: u32,
+
+    /// Total height of the hourglass.
+    #[arg(long, default_value_t = 12)]
+    height: u32,
+
+    /// Visual updates per second.
+    #[arg(long, default_value_t = 20.0)]
+    frames_per_sec: f64,
+
+    /// Simulation updates per visual update. (10 frames per sec * 5 steps per frame = 50 steps per sec)
+    #[arg(long, default_value_t = 2)]
+    steps_per_frame: u32,
+
+    /* TODO
+    /// Whether to flip the hourglass over once the time is elapsed.
+    #[arg(long, default_value_t = false)]
+    repeat: bool,
+    */
+
+    /// How much of the hourglass to fill with sand. 0 is no sand, 1 is completely fully.
+    #[arg(long, default_value_t = 0.75)]
+    fullness: f32
+}
+
+
+fn parse_timestamp(timestamp: &str) -> ParseResult<NaiveDateTime> {
+    Ok(NaiveDateTime::new(
+        chrono::Local::now().naive_local().date(),
+        NaiveTime::parse_from_str(timestamp, "%H:%M:%S").or_else(|_| {
+            NaiveTime::parse_from_str(timestamp, "%H:%M")
+        })?
+    ))
+}
+
+fn parse_time(time: &str) -> Result<TimeDelta, &'static str> {
+    fn try_parse_to_seconds(field: &str) -> Result<u64, &'static str> {
+        let chars: Vec<char> = field.chars().collect();
+        if chars.len() < 2 {
+            return Err("time part must be at least 2 chars long");
+        }
+
+        let (unit, number_chars): (&char, &[char]) = chars.split_last().expect("expected vector with length of at least 2 to have a last element");
+        let number: u64 = match number_chars.iter().collect::<String>().parse::<u64>() {
+            Ok(x) => x,
+            Err(_) => return Err("cannot parse time part number")
+        };
+
+        let multiplier = match *unit {
+            's' => 1,
+            'm' => 60,
+            'h' => 60 * 60,
+            'd' => 60 * 60 * 24,
+            'y' => 60 * 60 * 24 * 365,
+            _ => return Err("invalid time unit (valid units are s, d, h, d, and y)")
+        };
+
+        Ok(number * multiplier)
+    }
+
+    let mut total_seconds: u64 = 0;
+    for field in time.split_inclusive(|ch: char| !ch.is_digit(10)) {
+        total_seconds += try_parse_to_seconds(field)?;
+    }
+
+    Ok(TimeDelta::seconds(total_seconds.try_into().unwrap()))
+}
+
+
+struct TimeRange {
+    start: NaiveDateTime,
+    duration: TimeDelta
+}
+
+impl TimeRange {
+
+    pub fn try_from_args(begin: Option<NaiveDateTime>, end: Option<NaiveDateTime>, length: Option<TimeDelta>) -> Result<TimeRange, &'static str> {
+        let now = chrono::Local::now().naive_local();
+
+        match (&begin, &end, &length) {
+            (None, None, None) => Err("must define time range with some combination of `begin`, `end`, and `length`"),
+            (None, None, Some(length)) => Ok(TimeRange {
+                start: now,
+                duration: *length
+            }),
+            (None, Some(end), None) => Ok(TimeRange {
+                start: now,
+                duration: *end - now
+            }),
+            (None, Some(end), Some(length)) => Ok(TimeRange {
+                start: *end - *length,
+                duration: *length
+            }),
+            (Some(_), None, None) => Err("must provide duration with `end` or `length`"),
+            (Some(begin), None, Some(length)) => Ok(TimeRange {
+                start: *begin,
+                duration: *length
+            }),
+            (Some(begin), Some(end), None) => Ok(TimeRange {
+                start: *begin,
+                duration: if end > begin {
+                    *end - *begin
+                } else {
+                    (*end + TimeDelta::days(1)) - *begin
+                }
+            }),
+            (Some(begin), Some(end), Some(length)) => if (*end - *begin) == *length {
+                Ok(TimeRange {
+                    start: *begin,
+                    duration: *length
+                })
+            } else {
+                Err("`length` and `begin`..`end` must define the same duration")
+            },
+        }
+    }
+
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let time_range = TimeRange::try_from_args(
+        if let Some(begin_arg) = &args.begin { Some(parse_timestamp(begin_arg)?) } else { None },
+        if let Some(end_arg) = &args.end { Some(parse_timestamp(end_arg)?) } else { None },
+        if let Some(length_arg) = &args.length { Some(parse_time(length_arg)?) } else { None }
+    )?;
+
+    let mut glass = Hourglass::new(args.width.try_into().unwrap(), args.height.try_into().unwrap());
+    glass.fill_with_sand_from_top(args.fullness / 2.0);
+    glass.pinch();
+    glass.settle_state(&mut rand::rng());
 
     loop {
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear and go to top left corner
         println!("{}", glass);
 
-        if sand_to_add > 0 && glass.try_add_sand() {
-            sand_to_add -= 1;
-        }
+        let now = chrono::Local::now().naive_local();
+        let elapsed = now - time_range.start;
 
-        let moves = glass.advance(&mut rand::rng());
-        if moves == 0 {
-            no_moves_since += 1;
+        let time_progress: f64 = elapsed.num_milliseconds() as f64 / time_range.duration.num_milliseconds() as f64;
+
+        let top_sand = glass.count_top_sand();
+        let bottom_sand = glass.count_bottom_sand();
+        let sand_progress: f64 = if top_sand + bottom_sand != 0 {
+            bottom_sand as f64 / (top_sand + bottom_sand) as f64
         } else {
-            no_moves_since = 0;
+            0.0
+        };
+
+        if sand_progress < time_progress {
+            glass.unpinch();
+        } else {
+            glass.pinch();
         }
 
-        if no_moves_since >= 10 {
-            no_moves_since = 0;
-            glass.flip();
+        //println!("elapsed: {} sand: {} time: {}", elapsed, sand_progress, time_progress);
+        //println!("begin: {} duration: {} now: {}", time_range.start.format("%H:%M:%S"), time_range.duration, now.format("%H:%M:%S"));
+
+        // TODO stop simulating until next unpinch when steady state is reached
+        // TODO catch up when behind time
+        for _ in 0..args.steps_per_frame {
+            glass.advance(&mut rand::rng());
         }
 
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::thread::sleep(std::time::Duration::from_secs_f64(1.0 / args.frames_per_sec as f64));
     }
 }
